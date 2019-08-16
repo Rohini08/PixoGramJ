6083f45fc03a6a5763415609daa415a31be89227
Rohini <Rohini>
2019-08-16 17:45:45 +0530

    commit 2

--- src/main/java/com/cts/image/input/imageInput.java
+++ src/main/java/com/cts/image/input/imageInput.java
@@ -1,5 +1,7 @@
 package com.cts.image.input;
 
 public class imageInput {
+	
+	
 
 }
--- src/main/java/com/cts/user/input/userInput.java
+++ src/main/java/com/cts/user/input/userInput.java
@@ -1,5 +1,26 @@
 package com.cts.user.input;
 
+import java.util.Scanner;
+
+import com.cts.user.pojo.User;
+
 public class userInput {
+	Scanner s=new Scanner(System.in);
+	User user=new User();	
+	
+	public void input() {
+		System.out.println("Enter firstName");
+		 user.setFirstName(s.next());
+		 System.out.println("Enter lastName");
+		user.setLastName(s.next()); 
+		 System.out.println("Enter email");
+		 user.setEmail(s.next());
+		 System.out.println("Enter password");
+		user.setPassword(s.next()); 
+		 System.out.println("Enter profilePicture");
+		 user.setProfilePicture(s.next());
+		
+		
+	}
 
 }
